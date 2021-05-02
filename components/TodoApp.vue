5e27e372555d852bb9d8d68a357cc391eb5f09ec
jungsunhwan <znzn9292@gmail.com>
2021-05-03 01:52:44 +0900

    add components

---
+++ components/TodoApp.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <todo-item />
+        <todo-creator />
+    </div>
+</template>
+
+<script>
+import lowdb from 'lowdb'
+import LocalStorage from 'lowdb/adapters/LocalStorage'
+import TodoCreator from './TodoCreator'
+import TodoItem from './TodoItem'
+
+export default {
+    components: {
+        TodoCreator,
+        TodoItem
+    },
+    data () {
+        return {
+            db: null
+        }
+    },
+    created () {
+        this.initDB()
+    },
+    methods: {
+        initDB () {
+            const adapter = new LocalStorage('todo-app')
+            this.db = lowdb(adapter)
+
+            // Local DB 초기화
+            this.db.defaults({
+                todos: []   // Collection
+            })
+            .write()
+        }
+    }
+}
+</script>
